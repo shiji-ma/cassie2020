160c832e8625484f76a569371fa1e9853a24c2db
Shiji Ma <ma.shiji@hotmail.com>
2020-02-19 20:09:10 +1100

    Updates

---
+++ precache-manifest.b08e414d8da862d189e0536d6f03cc85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9aa9f42592cfd01c6eb49769cc19de1c",
+    "url": "/cassie2020/index.html"
+  },
+  {
+    "revision": "f3365f44df4defe625c5",
+    "url": "/cassie2020/static/css/main.e65de034.chunk.css"
+  },
+  {
+    "revision": "8b45388542428fbe9069",
+    "url": "/cassie2020/static/js/2.63ddfc82.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/cassie2020/static/js/2.63ddfc82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3365f44df4defe625c5",
+    "url": "/cassie2020/static/js/main.5358774e.chunk.js"
+  },
+  {
+    "revision": "cecd63d0febc1f81a088",
+    "url": "/cassie2020/static/js/runtime-main.e5a4c838.js"
+  }
+]);
